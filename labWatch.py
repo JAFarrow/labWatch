006f3a65b3ebac279657589ed37e2819c26508e8
JAFarrow <f.justin@tutanota.com>
2024-05-25 21:48:30 +0200

    Implemented basic commit check and notification

--- labWatch.py
+++ labWatch.py
@@ -1,19 +1,25 @@
 import datetime
 import os
-import requests
 import json
+import requests
+import subprocess
+
+
 
 userDataPath = f"{os.path.expanduser('~')}/.local/labWatch"
-userData = '/data.json'
-baseURL = "/api/v4/projects/"
+userDataFile = '/data.json'
+baseURL = "/api/v4/projects"
+userData = {}
+
 
 def initialize():
     os.makedirs(f"{userDataPath}", exist_ok=True)
-    if not os.path.isfile(f"{userDataPath}{userData}"):
+    if not os.path.isfile(userDataPath+userDataFile):
         init = getInfo()
-        init['lastCheck'] = datetime.datetime.now().isoformat()
-        with open(userDataPath+userData, "w") as file:
+        init['lastCheck'] = getFormattedTimeNow()
+        with open(userDataPath+userDataFile, "w") as file:
             json.dump(init, file)
+
 
 def getInfo():
     namespace = input("Please input your GitLab namespace: Enter for default 'gitlab.wethinkco.de' ")
@@ -24,5 +30,50 @@ def getInfo():
     projects = strProjects.split(",")
     return {'namespace': namespace, 'token': token, "projects": projects}
 
+
+def checkNewRequests():
+    message = ""
+    updatedProjects = 0
+    for project in userData["projects"]:
+        response = requests.get(f"https://{userData["namespace"]}{baseURL}/{project}/repository/commits?since={userData["lastCheck"]}&all=true",
+                                 headers={"PRIVATE-TOKEN": f"{userData["token"]}"})
+        if len(response.json()) > 0:
+            updatedProjects += 1
+            message += formatCommitMessage(response.json(), project)
+    userData["lastCheck"] = getFormattedTimeNow()
+    updateUserData()
+    subprocess.call(['notify-send', 'labWatch', message])
+
+
+
+def formatCommitMessage(jsonResponse, projectId):
+    nameResponse = requests.get(f"https://{userData["namespace"]}{baseURL}/{projectId}", 
+                                headers={"PRIVATE-TOKEN": f"{userData["token"]}"})
+    name = nameResponse.json()["name"]
+    if len(jsonResponse) == 1:
+        return f"{jsonResponse[0]["committer_name"]} pushed a commit to {name}."
+    else:
+        return f"Multiple commits to {name}."
+
+
+def getUserData():
+    global userData
+    with open(userDataPath+userDataFile, 'r') as file:
+        userData = json.load(file)
+
+
+def updateUserData():
+    global userData
+    with open(userDataPath+userDataFile, "w") as file:
+        json.dump(userData, file)
+
+
+def getFormattedTimeNow():
+    now = datetime.datetime.now(datetime.UTC)
+    return now.isoformat()
+
+
 if __name__ == '__main__':
     initialize()
+    getUserData()
+    checkNewRequests()
